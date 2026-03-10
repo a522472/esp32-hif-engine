9cc8c76ae48663c66da29a852fbbd46d99b3926b
a522472 <160769002+a522472@users.noreply.github.com>
2026-03-10 13:54:07 +0800

    Update hif_engine.c

--- hif_engine.c
+++ hif_engine.c
@@ -10,6 +10,7 @@ static inline uint32_t get_ccount(void) {
     return ccount;
 }
 
+// 接收 5 個參數：out_buf, in_buf, offset, cycle_ticks, stb_ticks
 static mp_obj_t hif_engine_play(size_t n_args, const mp_obj_t *args) {
     mp_buffer_info_t out_bufinfo;
     mp_buffer_info_t in_bufinfo;
@@ -17,13 +18,16 @@ static mp_obj_t hif_engine_play(size_t n_args, const mp_obj_t *args) {
     mp_get_buffer_raise(args[0], &out_bufinfo, MP_BUFFER_READ);
     mp_get_buffer_raise(args[1], &in_bufinfo, MP_BUFFER_WRITE);
     
-    uint32_t cycle_ticks = mp_obj_get_int(args[2]);
-    uint32_t stb_ticks   = mp_obj_get_int(args[3]);
+    uint32_t cap_idx     = mp_obj_get_int(args[2]);
+    uint32_t cycle_ticks = mp_obj_get_int(args[3]);
+    uint32_t stb_ticks   = mp_obj_get_int(args[4]);
 
-    // 陣列不再壓縮！長度就是實實在在的「總 CYCLE 數」
-    size_t total_cycles = in_bufinfo.len / sizeof(uint32_t);
+    // 因為 out_buf 是 [set, clr] 交替，所以長度要除以 2 才是總 Cycle 數
+    uint32_t total_cycles = (out_bufinfo.len / sizeof(uint32_t)) / 2;
     uint32_t *out_data = (uint32_t *)out_bufinfo.buf;
-    uint32_t *in_data  = (uint32_t *)in_bufinfo.buf;
+    
+    uint32_t in_length = in_bufinfo.len / sizeof(uint32_t);
+    uint32_t *in_data = (uint32_t *)in_bufinfo.buf;
 
     volatile uint32_t *gpio_w1ts = (volatile uint32_t *)GPIO_OUT_W1TS_REG;
     volatile uint32_t *gpio_w1tc = (volatile uint32_t *)GPIO_OUT_W1TC_REG;
@@ -35,32 +39,30 @@ static mp_obj_t hif_engine_play(size_t n_args, const mp_obj_t *args) {
     uint32_t next_cycle_time = get_ccount();
 
     // 🚀 暴力的等時展開迴圈 (Constant-Time Loop)
-    for (size_t i = 0; i < total_cycles; i++) {
-        // 1. 讀取這個 CYCLE 要做的動作
+    for (uint32_t i = 0; i < total_cycles; i++) {
         uint32_t set_m = out_data[out_idx++];
         uint32_t clr_m = out_data[out_idx++];
 
-        // 2. 暴力寫入
         *gpio_w1tc = clr_m;
         *gpio_w1ts = set_m;
 
-        // 3. 死等 STB 點
         uint32_t target_stb = next_cycle_time + stb_ticks;
         while ((int32_t)(get_ccount() - target_stb) < 0) { }
 
-        // 4. 暴力讀取
-        in_data[i] = *gpio_in;
+        // 採樣並寫入記憶體 (加上防越界保護)
+        if (cap_idx < in_length) {
+            in_data[cap_idx] = *gpio_in;
+        }
+        cap_idx++;
 
-        // 5. 死等下一個 CYCLE 的起點
         next_cycle_time += cycle_ticks;
         while ((int32_t)(get_ccount() - next_cycle_time) < 0) { }
     }
     
-    return mp_const_none;
+    return mp_obj_new_int(cap_idx);
 }
 
-// 改回 4 個參數 (out_buf, in_buf, cycle_ticks, stb_ticks)
-static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(hif_engine_play_obj, 4, 4, hif_engine_play);
+static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(hif_engine_play_obj, 5, 5, hif_engine_play);
 
 mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw, mp_obj_t *args) {
     MP_DYNRUNTIME_INIT_ENTRY
