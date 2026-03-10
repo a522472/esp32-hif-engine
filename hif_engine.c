b6a9fa554d5871e7e82dc4166e23acbd879bca18
a522472 <160769002+a522472@users.noreply.github.com>
2026-03-10 11:15:48 +0800

    Update hif_engine.c

--- hif_engine.c
+++ hif_engine.c
@@ -4,6 +4,13 @@
 #define GPIO_OUT_W1TC_REG 0x6000400C
 #define GPIO_IN_REG       0x6000403C
 
+// 👑 黑科技：直接讀取 ESP32 內部的絕對時脈計數器 (精準度 4.16ns)
+static inline uint32_t get_ccount(void) {
+    uint32_t ccount;
+    __asm__ __volatile__("rsr %0, ccount" : "=r"(ccount));
+    return ccount;
+}
+
 static mp_obj_t hif_engine_play(size_t n_args, const mp_obj_t *args) {
     mp_buffer_info_t out_bufinfo;
     mp_buffer_info_t in_bufinfo;
@@ -11,13 +18,11 @@ static mp_obj_t hif_engine_play(size_t n_args, const mp_obj_t *args) {
     mp_get_buffer_raise(args[0], &out_bufinfo, MP_BUFFER_READ);
     mp_get_buffer_raise(args[1], &in_bufinfo, MP_BUFFER_WRITE);
     
-    uint32_t pre_delay = mp_obj_get_int(args[2]);
-    uint32_t post_delay = mp_obj_get_int(args[3]);
+    // 現在 Python 傳進來的是「絕對 Ticks 數量」，而不是迴圈次數
+    uint32_t cycle_ticks = mp_obj_get_int(args[2]);
+    uint32_t stb_ticks = mp_obj_get_int(args[3]);
 
-    // 💡 升級為 32 位元：長度計算要除以 sizeof(uint32_t)，再除以 2 (因為是 set, clr 兩兩一組)
     size_t length = (out_bufinfo.len / sizeof(uint32_t)) / 2;
-    
-    // 💡 指標升級為 uint32_t，才能容納高達 2048 的腳位遮罩！
     uint32_t *out_data = (uint32_t *)out_bufinfo.buf;
     uint32_t *in_data = (uint32_t *)in_bufinfo.buf;
 
@@ -26,23 +31,26 @@ static mp_obj_t hif_engine_play(size_t n_args, const mp_obj_t *args) {
     volatile uint32_t *gpio_in   = (volatile uint32_t *)GPIO_IN_REG;
 
     size_t out_idx = 0;
+
+    // 🚀 進入硬體領域
     for (size_t i = 0; i < length; i++) {
-        // 💡 讀取出來的狀態也升級為 32 位元
+        // 1. 紀錄這個 CYCLE 開始的「絕對時間戳記」
+        uint32_t start_tick = get_ccount();
+
+        // 2. 記憶體讀取與 GPIO 輸出
         uint32_t set_m = out_data[out_idx++];
         uint32_t clr_m = out_data[out_idx++];
-
         *gpio_w1tc = clr_m;
         *gpio_w1ts = set_m;
 
-        for (volatile uint32_t d = 0; d < pre_delay; d++) {
-            __asm__ __volatile__("nop");
-        }
+        // 3. 盯著時鐘看，時間不到 STB 點絕對不往下走！(完美吸收所有記憶體延遲)
+        while ((get_ccount() - start_tick) < stb_ticks) { }
 
+        // 4. 精準採樣
         in_data[i] = *gpio_in;
 
-        for (volatile uint32_t d = 0; d < post_delay; d++) {
-            __asm__ __volatile__("nop");
-        }
+        // 5. 盯著時鐘看，等待這個 CYCLE 完美結束
+        while ((get_ccount() - start_tick) < cycle_ticks) { }
     }
     return mp_const_none;
 }
